fc1a805eb994be017db3502c3e5b2ee8d490f940
JZX <505684228@qq.com>
2019-06-30 17:25:21 +0800

    add Indent with Post

--- src/main/java/cn/edu/cqu/yihao/pojo/IndentWithPost.java
+++ src/main/java/cn/edu/cqu/yihao/pojo/IndentWithPost.java
@@ -1,5 +1,28 @@
 package cn.edu.cqu.yihao.pojo;
 
 public class IndentWithPost {
-
+	private Indent indent = null;
+	private Post post = null;
+	private int havePost = 0;
+	
+	
+	public Indent getIndent() {
+		return indent;
+	}
+	public void setIndent(Indent indent) {
+		this.indent = indent;
+	}
+	public Post getPost() {
+		return post;
+	}
+	public void setPost(Post post) {
+		this.post = post;
+	}
+	public int getHavePost() {
+		return havePost;
+	}
+	public void setHavePost(int havePost) {
+		this.havePost = havePost;
+	}
+	
 }
