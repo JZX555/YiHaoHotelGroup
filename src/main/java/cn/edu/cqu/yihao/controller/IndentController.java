f805576a8e27620376f21b8d11f037dfdb904a98
lijiayi <1023511971@qq.com>
2019-07-01 10:17:36 +0800

    add IndentController

---
+++ src/main/java/cn/edu/cqu/yihao/controller/IndentController.java
@@ -0,0 +1,172 @@
+package cn.edu.cqu.yihao.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.edu.cqu.yihao.pojo.Account;
+import cn.edu.cqu.yihao.pojo.Indent;
+import cn.edu.cqu.yihao.pojo.IndentWithPost;
+import cn.edu.cqu.yihao.pojo.Post;
+import cn.edu.cqu.yihao.service.AccountService;
+import cn.edu.cqu.yihao.service.IndentService;
+import cn.edu.cqu.yihao.service.PostService;
+
+@Controller
+@RequestMapping("/indents")
+public class IndentController {
+	
+	@Autowired
+	private PostService pService;
+	@Autowired
+	private IndentService inService;
+	@Autowired
+	private AccountService acService;
+	
+	
+	@RequestMapping("/show_indents1")  
+	@ResponseBody
+	public List<IndentWithPost> show_indents1(HttpServletRequest request,Model model,@CookieValue("loginTel") String tel) {//我的订单 页面
+		Account account = acService.getAccountByTel(tel);  //显示已付款订单
+		Indent[] paid_indents = inService.getByTypeandTel(1, tel);
+		Indent[] checkin_indents =inService.getByTypeandTel(2, tel);
+		List<IndentWithPost> default_indents = new ArrayList<>();
+		for(int i =0;i<paid_indents.length;i++ ) {
+			IndentWithPost iwp=new IndentWithPost();
+			iwp.setIndent(paid_indents[i]);
+			String indent_id=paid_indents[i].getIndentId();
+			Post post=pService.getById(indent_id);
+			if(post!=null) {
+				iwp.setPost(post);
+				iwp.setHavePost(1);
+			}
+			
+			default_indents.add(iwp);
+		}
+		for(int i =0;i<checkin_indents.length;i++ ) {
+			IndentWithPost iwp=new IndentWithPost();
+			iwp.setIndent(checkin_indents[i]);
+			String indent_id=checkin_indents[i].getIndentId();
+			Post post=pService.getById(indent_id);
+			if(post!=null) {
+				iwp.setPost(post);
+				iwp.setHavePost(1);
+			}
+			
+			default_indents.add(iwp);
+		}
+		
+		return default_indents;	
+	}
+	@RequestMapping("/show_indents2")  
+	@ResponseBody
+	public List<IndentWithPost> show_indents2(HttpServletRequest request,Model model,@CookieValue("loginTel") String tel) {//我的订单 页面
+		Account account = acService.getAccountByTel(tel);  //显示已完成订单
+		Indent[] finished_indents = inService.getByTypeandTel(3, tel);
+		List<IndentWithPost> default_indents = new ArrayList<>();
+		for(int i =0;i<finished_indents.length;i++ ) {
+			IndentWithPost iwp=new IndentWithPost();
+			iwp.setIndent(finished_indents[i]);
+			String indent_id=finished_indents[i].getIndentId();
+			Post post=pService.getById(indent_id);
+			if(post!=null) {
+				iwp.setPost(post);
+				iwp.setHavePost(1);
+			}
+			
+			default_indents.add(iwp);
+		}
+		return default_indents;	
+	}
+	@RequestMapping("/show_indents3")  
+	@ResponseBody
+	public List<IndentWithPost> show_indents3(HttpServletRequest request,Model model,@CookieValue("loginTel") String tel) {//我的订单 页面
+		Account account = acService.getAccountByTel(tel);  //显示未付款订单
+		Indent[] notpay_indents = inService.getByTypeandTel(0, tel);
+		List<IndentWithPost> default_indents = new ArrayList<>();
+
+		for(int i =0;i<notpay_indents.length;i++ ) {
+			IndentWithPost iwp=new IndentWithPost();
+			iwp.setIndent(notpay_indents[i]);
+			String indent_id=notpay_indents[i].getIndentId();
+			Post post=pService.getById(indent_id);
+			if(post!=null) {
+				iwp.setPost(post);
+				iwp.setHavePost(1);
+			}
+			
+			default_indents.add(iwp);
+		}
+		return default_indents;	
+	}
+	@RequestMapping("/show_indents4")  
+	@ResponseBody
+	public List<IndentWithPost> show_indents4(HttpServletRequest request,Model model,@CookieValue("loginTel") String tel) {//我的订单 页面
+		Account account = acService.getAccountByTel(tel);  //显示已完成和已退款订单
+		Indent[] fail_indents = inService.getByTypeandTel(4, tel);
+		Indent[] refund_indents =inService.getByTypeandTel(5, tel);
+		List<IndentWithPost> default_indents = new ArrayList<>();
+		for(int i =0;i<fail_indents.length;i++ ) {
+			IndentWithPost iwp=new IndentWithPost();
+			iwp.setIndent(fail_indents[i]);
+			String indent_id=fail_indents[i].getIndentId();
+			Post post=pService.getById(indent_id);
+			if(post!=null) {
+				iwp.setPost(post);
+				iwp.setHavePost(1);
+			}
+			
+			default_indents.add(iwp);
+		}
+		for(int i =0;i<refund_indents.length;i++ ) {
+			IndentWithPost iwp=new IndentWithPost();
+			iwp.setIndent(refund_indents[i]);
+			String indent_id=refund_indents[i].getIndentId();
+			Post post=pService.getById(indent_id);
+			if(post!=null) {
+				iwp.setPost(post);
+				iwp.setHavePost(1);
+			}
+			
+			default_indents.add(iwp);
+		}
+		return default_indents;	
+	}
+	
+
+	@RequestMapping("/addComment")//添加评价
+	@ResponseBody
+	public IndentWithPost addComment(HttpServletRequest request, Model model) {
+		String indent_id=(String)request.getParameter("indent_id");//创建一条评论
+		Double score=Double.parseDouble((String)request.getParameter("score"));
+		String comment=(String)request.getParameter("comment");
+		int res=pService.addPost(indent_id, score, comment);
+		Indent indent=inService.getById(indent_id); //更新该条订单对应的IndentWithPost结构
+		Post post=pService.getById(indent_id);
+		IndentWithPost iwp=new IndentWithPost();
+		iwp.setIndent(indent);
+		iwp.setPost(post);
+		iwp.setHavePost(1);
+		
+		return iwp;		
+	}
+	@RequestMapping("/showComment")//查看评价
+	public String changeComment(HttpServletRequest request, Model model) {
+		String indent_id=(String)request.getParameter("indent_id");
+		Post p=pService.getById(indent_id);
+		model.addAttribute("score",p.getSocre());
+		model.addAttribute("comment", p.getComment());
+		
+		return "/showComment";		
+	}
+	
+	
+}
--- src/main/java/cn/edu/cqu/yihao/controller/PayController.java
+++ src/main/java/cn/edu/cqu/yihao/controller/PayController.java
@@ -5,12 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.edu.cqu.yihao.pojo.Account;
 import cn.edu.cqu.yihao.pojo.Debit_card;
+import cn.edu.cqu.yihao.pojo.Indent;
 import cn.edu.cqu.yihao.service.AccountService;
 import cn.edu.cqu.yihao.service.Debit_cardService;
+import cn.edu.cqu.yihao.service.IndentService;
 
 @Controller("/pay")
 public class PayController {
@@ -19,11 +22,13 @@ public class PayController {
 	private Debit_cardService dbcService;
 	@Autowired
 	private AccountService acService;
-	@RequestMapping("/pay")
-	public String chosePay(HttpServletRequest req, Model model) {
-		String tel=(String)req.getParameter("tel");
+	@Autowired
+	private IndentService inService;
+	
+	@RequestMapping("/choose")
+	public String choosePay(HttpServletRequest req, Model model,@CookieValue("loginTel") String tel) {
 		Account account=acService.getAccountByTel(tel);
-		Double  price = Double.parseDouble((String)req.getParameter("price"));
+		Double  price = Double.parseDouble((String)req.getParameter("price"));//获得价格
 		int need_point=new Double(price).intValue();
 		int point=account.getPoint();
 		
@@ -37,40 +42,73 @@ public class PayController {
 		else {
 			model.addAttribute("price",price);
 			return "debit_card";
-		}
-		
-		
+		}	
 	}
 	
 	
 	@RequestMapping("/debit_card")
-	public int debit_cardWay(HttpServletRequest req, Model model) {
+	public int debit_cardWay(HttpServletRequest req, Model model,@CookieValue("loginTel") String tel) {
 		int resflag=0;
-		Double  price = Double.parseDouble((String)req.getParameter("price"));
-		String tel=(String)req.getParameter("tel");
+		Double  price = Double.parseDouble((String)req.getParameter("price"));//需要支付的价格
+		String indent_id=(String)req.getParameter("indent_id");
+		Indent indent= new Indent();
+		indent.setIndentId(indent_id);		
+		Account account=acService.getAccountByTel(tel);
+		
 		String card_id=(String)req.getParameter("card_id");
-		int rows=dbcService.sub(card_id,price);
-		if(rows==0)
-			return resflag;
-		else {
+		int rows=dbcService.sub(card_id,price); //信用卡减钱
+		if(rows==0) { //支付失败
+			indent.setIndentType(0);
+			inService.updateSelect(indent);
+			return resflag;}
+		else {  //支付成功
+			indent.setIndentType(1);//更新支付状态
+			inService.updateSelect(indent);
+			
 			int point=new Double(price).intValue();
-			int r=acService.addPoint(tel, point);
+			int r=acService.addPoint(tel, point); //增加积分
+			int curpoint=account.getMaxpoint();  //更新会员等级
+			if(curpoint<10000)
+				acService.setVipLevel(tel,0);
+			else if(curpoint>=10000 && curpoint<30000)
+				acService.setVipLevel(tel,1);
+			else if(curpoint>=30000 && curpoint<100000)
+				acService.setVipLevel(tel,2);
+			else
+				acService.setVipLevel(tel,3);
 			resflag=1;
 			return resflag;
 		}
 	
 	}
 	@RequestMapping("/point")
-	public int pointWay(HttpServletRequest req, Model model) {
+	public int pointWay(HttpServletRequest req, Model model,@CookieValue("loginTel") String tel) {
 		int resflag=0;
-		String s=(String)req.getParameter("need_point");
+		String s=(String)req.getParameter("need_point"); //need_point是十倍的price
 		int need_point=Integer.parseInt(s);
-		String tel=(String)req.getParameter("tel");
-		int rows=acService.subPoint(tel, need_point);
-		if(rows==0)
-			return resflag;
+		Account account=acService.getAccountByTel(tel);
+		String indent_id=(String)req.getParameter("indent_id");
+		Indent indent= new Indent();
+		indent.setIndentId(indent_id);
+		
+		int rows=acService.subPoint(tel, need_point); //减积分
+		if(rows==0) {//支付失败
+			indent.setIndentType(0);
+			inService.updateSelect(indent);
+			return resflag;}
 		else {
+			indent.setIndentType(1);  //更新支付状态
+			inService.updateSelect(indent);
 			int r=acService.addPoint(tel, need_point/10);
+			int curpoint=account.getMaxpoint();  //修改会员等级
+			if(curpoint<10000)
+				acService.setVipLevel(tel,0);
+			else if(curpoint>=10000 && curpoint<30000)
+				acService.setVipLevel(tel,1);
+			else if(curpoint>=30000 && curpoint<100000)
+				acService.setVipLevel(tel,2);
+			else
+				acService.setVipLevel(tel,3);
 			resflag=1;
 			return resflag;
 		}
@@ -78,3 +116,4 @@ public class PayController {
 	
 
 }
+
